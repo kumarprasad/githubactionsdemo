97497ee48c9ce7cc8666e5b3da7ec587c0dc0ed4
kumarprasad <kumar_prasad1987@yahoo.co.in>
2022-08-08 11:05:05 +0530

    added files

---
+++ movie-catalog-service/src/main/java/io/kmrbarins/moviecatalogservice/HomeController.java
@@ -0,0 +1,22 @@
+package io.kmrbarins.moviecatalogservice;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+@RestController
+public class HomeController {
+	
+	@Autowired
+	private RestTemplate restTemplate;
+	@GetMapping("/home")
+	public String home() {
+		
+		String products = restTemplate.getForObject("http://PRODUCT-MIROSERVICES/products", String.class);
+		return products;
+		
+		//return "homepage";
+	}
+
+}
